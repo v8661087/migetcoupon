5b8ecdc90a53832bac1137d11fb0c03c09c51a5a
v8661087 <45732261+v8661087@users.noreply.github.com>
2019-02-13 16:13:06 +0800

    Update migetcoupon.py

--- migetcoupon.py
+++ migetcoupon.py
@@ -4,24 +4,24 @@ import time
 
 browser = webdriver.Chrome()
 def login(name,pwd):
-    browser.get( 'https://account.xiaomi.com/')#登录网址
+    browser.get( 'https://account.xiaomi.com/')#登錄網址
     time.sleep(1)
-    browser.find_element_by_id("username").send_keys(name) #利用账号标签的ID，确定位置并send信息
-    browser.find_element_by_id("pwd").send_keys(pwd) #利用密码标签的ID，确定位置并send信息
-    browser.find_element_by_id("login-button").click()#利用登录按钮的ID，确定位置并点击
-    #如果找不到标签ID，可以使用其他方法来确定元素位置
+    browser.find_element_by_id("username").send_keys(name) #利用帳號標籤的ID，確定位置並send信息
+    browser.find_element_by_id("pwd").send_keys(pwd) #利用密碼標籤的ID，確定位置並send信息
+    browser.find_element_by_id("login-button").click()#利用登錄按鈕的ID，確定位置並點擊
+    #如果找不到標籤ID，可以使用其他方法來確定元素位置
     time.sleep(1)
-    browser.get("https://event.mi.com/tw/sales2018/super-sales-day")#切换到秒杀页面
+    browser.get("https://event.mi.com/tw/sales2018/super-sales-day")#切換到頁面
     
 
 def keepclick():
     while True:   #迴圈
-        browser.find_element_by_xpath("//*[@id='sec_1']/div/div/ul/li/ol/li[4]/a/span").click()  #购买按钮的Xpath
+        browser.find_element_by_xpath("//*[@id='sec_1']/div/div/ul/li/ol/li[4]/a/span").click()  #按鈕的Xpath
         print("hit")
         
 
 def main():
-    login('你的帳號','你的帳號')
+    login('name','pwd') #name為你的帳號，pwd為你的密碼
     keepclick()
     
 

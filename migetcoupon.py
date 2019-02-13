82298586218e36db427f4cbab1d838b6d76adbf0
v8661087 <45732261+v8661087@users.noreply.github.com>
2019-02-13 16:24:50 +0800

    Update migetcoupon.py

--- migetcoupon.py
+++ migetcoupon.py
@@ -6,10 +6,9 @@ browser = webdriver.Chrome()
 def login(name,pwd):
     browser.get( 'https://account.xiaomi.com/')#登錄網址
     time.sleep(1)
-    browser.find_element_by_id("username").send_keys(name) #利用帳號標籤的ID，確定位置並send信息
-    browser.find_element_by_id("pwd").send_keys(pwd) #利用密碼標籤的ID，確定位置並send信息
+    browser.find_element_by_id("username").send_keys(name) #利用帳號標籤的ID，確定位置並輸入
+    browser.find_element_by_id("pwd").send_keys(pwd) #利用密碼標籤的ID，確定位置並輸入
     browser.find_element_by_id("login-button").click()#利用登錄按鈕的ID，確定位置並點擊
-    #如果找不到標籤ID，可以使用其他方法來確定元素位置
     time.sleep(1)
     browser.get("https://event.mi.com/tw/sales2018/super-sales-day")#切換到頁面
     
@@ -21,7 +20,7 @@ def keepclick():
         
 
 def main():
-    login('name','pwd') #name為你的帳號，pwd為你的密碼
+    login('name','pwd') #在此輸入name為你的帳號，pwd為你的密碼
     keepclick()
     
 
